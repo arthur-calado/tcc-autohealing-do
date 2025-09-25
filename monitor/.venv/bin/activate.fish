0a63966220f9ed0fbfca2fbda76690215874dee3
Arthur <arthur.calado@ufpe.br>
2025-09-25 11:14:56 -0300

    rename rep

--- monitor/.venv/bin/activate.fish
+++ monitor/.venv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV /home/calado/tcc/tcc-autohealing-do/monitor/.venv
+set -gx VIRTUAL_ENV /home/calado/tcc/autohealing-do/monitor/.venv
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/"bin $PATH
