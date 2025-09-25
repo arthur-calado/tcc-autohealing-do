cd4531eb95217f9075863acefd0aabcf9ff8ec31
Arthur Calado <arthur.calado@ufpe.br>
2025-09-24 23:54:08 -0300

    update structure

--- terraform/monitoring.tf
+++ terraform/monitoring.tf
@@ -1,14 +1,21 @@
 # terraform/monitoring.tf
 
-# Droplet para rodar o Prometheus, Alertmanager e o Healer
 resource "digitalocean_droplet" "monitoring" {
   name     = "${var.project_name}-monitoring"
-  size     = "s-1vcpu-2gb" # Recomendado um pouco mais de memória
+  size     = "s-1vcpu-2gb"
   image    = var.image
   region   = var.region
   vpc_uuid = digitalocean_vpc.tcc_vpc.id
   ssh_keys = var.ssh_keys
 
-  # Usamos o script de user_data para provisionar tudo
-  user_data = file("${path.module}/user_data_monitor.sh.tftpl")
-} 
+  # AQUI ESTA A CORRECAO:
+  # Adicionamos a linha "do_token = var.do_token" ao template principal
+  # para que ele também receba a variável.
+  user_data = templatefile("${path.module}/user_data_monitor.sh.tftpl", {
+    prometheus_config = templatefile("${path.module}/prometheus.yml.tftpl", {
+      do_token = var.do_token
+    })
+    rules_config = file("${path.module}/rules.yml.tftpl")
+    do_token     = var.do_token # <-- A LINHA QUE FALTAVA
+  })
+}
